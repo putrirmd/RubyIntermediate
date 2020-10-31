7620f9a9290555d7324437c28cb225152d7d5b85
putrirmd <72644073+putrirmd@users.noreply.github.com>
2020-10-31 22:12:13 +0700

    Update db_connector.rb

--- homework_3/db_connector.rb
+++ homework_3/db_connector.rb
@@ -5,9 +5,9 @@ require './category.rb'
 def create_db_client
     client = Mysql2::Client.new(
         :host => '127.0.0.1',
-        :username => 'root',
-        :password => 'Intern@l2301',
-        :database => 'food_oms_db'
+        :username => 'your username',
+        :password => 'your password',
+        :database => 'your food database'
     )
     client
 end
